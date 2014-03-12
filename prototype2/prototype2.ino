e70ee3d236cf26416a89b99d38541a43aba24618
Benoit Landry <landry@mit.edu>
2014-03-11 22:32:55 -0400

    stepper + sonar

--- prototype2/prototype2.ino
+++ prototype2/prototype2.ino
@@ -1,1 +1,65 @@
+#include <Stepper.h>
+
+const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+// for your motor
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 3, 4, 5, 6);
+
+// this constant won't change.  It's the pin number
+// of the sensor's output:
+const int pingPin = 2;
+
+int stepperSpeed = 30; // rpm
+int steps = 1;
+long distance;
+
+void setup() {
+  // set the speed at 60 rpm:
+  myStepper.setSpeed(stepperSpeed);
+}
+
+void loop()
+{
+
+  distance = readDistance();
+  
+  myStepper.step(1);
+
+}
+
+long readDistance()
+{ 
+  // establish variables for duration of the ping,
+  // and the distance result in inches and centimeters:
+  long duration, cm;
+
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+
+  // The same pin is used to read the signal from the PING))): a HIGH
+  // pulse whose duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+  
+  // convert the time into a distance
+  cm = microsecondsToCentimeters(duration);
+  
+  return cm;
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
 
