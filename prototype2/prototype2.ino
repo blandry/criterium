8e1ab93fe78bafde5080af2ad4ba2d5c4ba21929
Benoit Landry <landry@mit.edu>
2014-03-12 09:50:17 -0400

    speed changes

--- prototype2/prototype2.ino
+++ prototype2/prototype2.ino
@@ -1,65 +1,62 @@
 #include <Stepper.h>
+#include <Servo.h> 
 
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
-// for your motor
+Servo myservo1;
+Servo myservo2;
+int pos = 55; 
+int direction = 1;
 
-// initialize the stepper library on pins 8 through 11:
+const int stepsPerRevolution = 200;
 Stepper myStepper(stepsPerRevolution, 3, 4, 5, 6);
-
-// this constant won't change.  It's the pin number
-// of the sensor's output:
-const int pingPin = 2;
-
-int stepperSpeed = 30; // rpm
+int stepperSpeed = 10; // rpm
 int steps = 1;
+
+const int pingPin = 2;
 long distance;
 
 void setup() {
-  // set the speed at 60 rpm:
   myStepper.setSpeed(stepperSpeed);
+  myservo1.attach(8); 
+  myservo2.attach(9);  
 }
 
 void loop()
 {
-
-  distance = readDistance();
-  
-  myStepper.step(1);
-
+  for(pos = 55; pos <= 125; pos += 1) 
+  {                                 
+    myservo1.write(pos);
+    myservo2.write(pos);
+    distance = readDistance();
+    myStepper.setSpeed(distance);
+    myStepper.step(1);    
+  } 
+  for(pos = 125; pos>=55; pos-=1) 
+  {                                
+    myservo1.write(pos);
+    myservo2.write(pos); 
+    distance = readDistance();
+    myStepper.setSpeed(distance);
+    myStepper.step(1);
+  } 
 }
 
 long readDistance()
 { 
-  // establish variables for duration of the ping,
-  // and the distance result in inches and centimeters:
   long duration, cm;
-
-  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
   pinMode(pingPin, OUTPUT);
   digitalWrite(pingPin, LOW);
   delayMicroseconds(2);
   digitalWrite(pingPin, HIGH);
   delayMicroseconds(5);
   digitalWrite(pingPin, LOW);
-
-  // The same pin is used to read the signal from the PING))): a HIGH
-  // pulse whose duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
   pinMode(pingPin, INPUT);
   duration = pulseIn(pingPin, HIGH);
-  
-  // convert the time into a distance
   cm = microsecondsToCentimeters(duration);
-  
   return cm;
 }
 
 long microsecondsToCentimeters(long microseconds)
 {
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the
-  // object we take half of the distance travelled.
   return microseconds / 29 / 2;
 }
 
